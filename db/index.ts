5171898525ddf170ed4c0ff9ba44327aef3a4060
Trevor Smith <trevorsmith@onetable.org>
2025-12-19 12:56:00 -0800

    use websocket drizzle connection

--- db/index.ts
+++ db/index.ts
@@ -1,13 +1,9 @@
-import { drizzle as drizzleNeon } from 'drizzle-orm/neon-http'
-import { drizzle as drizzlePostgres } from 'drizzle-orm/node-postgres'
-import { neon } from '@neondatabase/serverless'
-
+import { drizzle } from 'drizzle-orm/neon-serverless'
+import { Pool } from '@neondatabase/serverless'
 import * as schema from './schema'
 
-export const db = process.env.VERCEL
-  ? drizzleNeon({
-      client: neon(process.env.DATABASE_URL!),
-      schema,
-      casing: 'snake_case',
-    })
-  : drizzlePostgres(process.env.DATABASE_URL!, { schema, casing: 'snake_case' })
+if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL environment variable is required')
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL })
+
+export const db = drizzle(pool, { schema, casing: 'snake_case' })
