d84ae223840a9239bd64e95bf2f13e9de10103cd
Trevor Smith <trevorsmith@onetable.org>
2025-12-20 14:58:05 -0800

    add wizard and progress components

--- app/(portal)/profile/components/Profile/components/ProfileContent.tsx
+++ app/(portal)/profile/components/Profile/components/ProfileContent.tsx
@@ -5,10 +5,8 @@ import { ProfileContext } from '../store/provider'
 
 export const ProfileContent = () => {
   const { profile, state } = use(ProfileContext)
-  console.log('profile:', profile, 'state:', state)
+
   return (
-    <div>
-      Profile Content
-    </div> 
+    <div />
   )
 }
--- app/(portal)/profile/components/Wizard/index.tsx
+++ app/(portal)/profile/components/Wizard/index.tsx
@@ -1,13 +1,20 @@
 'use client'
 
 import { use } from 'react'
+import { Button } from '@/app/components/Button'
+import { WizardActionType } from './store/types'
 import { WizardContext } from './store/provider'
+import { Progress } from './components/Progress'
 
 export const Wizard = () => {
-  const { state } = use(WizardContext)
-
-  console.log('Wizard State:', state)
+  const { dispatch } = use(WizardContext)
   return (
-    <div>Wizard Component</div>
+    <div>
+      <Progress />
+      <div className="flex justify-between">
+        <Button variant="outline" onClick={() => dispatch({ type: WizardActionType.PreviousStep })}>Previous Page</Button>
+        <Button onClick={() => dispatch({ type: WizardActionType.NextStep })}>Next Page</Button>
+      </div>
+    </div>
   )
 }
