f558f7ceb324ca80d9e083472fc681b2a51716bf
Trevor Smith <trevorsmith@onetable.org>
2026-01-18 12:18:08 -0800

    use actions in api issues routes

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,8 +1,6 @@
-import { eq } from 'drizzle-orm'
-import { db } from '@/db'
-import { issues } from '@/db/schema'
 import { NextRequest, NextResponse } from 'next/server'
-import { getAuthenticatedUser } from '@/dal/user'
+import { getIssues } from '@/dal/issue'
+import { createIssue } from '@/actions/issues'
 
 /*
 * GET /api/issues
@@ -10,10 +8,7 @@ import { getAuthenticatedUser } from '@/dal/user'
 */
 export const GET = async () => {
   try {
-    const user = await getAuthenticatedUser()
-    if (!user) return NextResponse.json({ success: false, error: 'Unauthorized' }, { status: 401 })
-
-    const userIssues = await db.query.issues.findMany({ where: eq(issues.userId, user.id) })
+    const userIssues = await getIssues()
 
     return NextResponse.json({ success: true, data: { issues: userIssues } }, { status: 200 })
   } catch (e) {
@@ -28,13 +23,12 @@ export const GET = async () => {
 */
 export const POST = async (req: NextRequest) => {
   try {
-    const user = await getAuthenticatedUser()
-    if (!user) return NextResponse.json({ success: false, error: 'Unauthorized' }, { status: 401 })
-
     const values = await req.json()
-    const [newIssue] = await db.insert(issues).values({ userId: user?.id, ...values }).returning()
+    const result = await createIssue(values)
 
-    return NextResponse.json({ success: true, data: newIssue }, { status: 201 })
+    if (!result.success) return NextResponse.json(result, { status: 400 })
+
+    return NextResponse.json(result, { status: 201 })
   } catch (e) {
     console.error(e)
     return NextResponse.json({ success: false, error: 'Application Error' }, { status: 500 })
